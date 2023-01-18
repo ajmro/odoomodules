64528fa6a330e921ad8e2a1f00c4b13f95ef05f9
ajmro <livres@tuta.io>
2023-01-17 22:32:16 -0400

    Update

--- odoo_academy/models/product_template.py
+++ odoo_academy/models/product_template.py
@@ -1,7 +1,6 @@
-from odoo import api, fields, models, exceptions
+from odoo import api, fields, models
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     is_session_product = fields.Boolean('Use as session product', help='Check to use this as a Product for Session Fee', default=False)
-    
